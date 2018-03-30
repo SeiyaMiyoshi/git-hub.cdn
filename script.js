d60bc7c7a56590c3a5ef2b23cbb87de66e7851c5
SeiyaMiyoshi <37858628+SeiyaMiyoshi@users.noreply.github.com>
2018-03-30 15:49:57 +0900

    Update script.js

--- script.js
+++ script.js
@@ -9,4 +9,21 @@
     $('#myTable').dataTable();
     });
   };
+  
+	$.ready(function() {
+	    var table = $('#employee_list').DataTable( {
+	        deferRender:    true,
+	        scrollY:        200,
+	        scrollX:        true,
+	        scrollCollapse: true,
+	        scroller:       true
+	    } );
+	 
+        new $.fn.dataTable.FixedColumns( table, {
+          leftColumns: 2
+        });
+    });
+      
+  
 })( jQuery );
+
